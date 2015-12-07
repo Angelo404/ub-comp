e6aa238c431fc05c4942e27c366afc88e13a9dac
Angelo404 <e.karountzos@hotmail.com>
2015-12-07 16:47:01 +0100

    initializing repo

---
+++ src/State.py
@@ -0,0 +1,16 @@
+
+
+class State:
+	
+	def __init__(self):
+
+
+		self.LightLevel = 0	# RANGE NEEDS TO BE SET
+		self.IsLightOn = False
+		self.AuthPeople = []
+		self.Alarms = [] 	# FORMAT NEEDS TO BE DEFINED
+		self.IsMediaPLaying = False
+
+
+
+
