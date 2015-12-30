34b556da24dad4f7807bafcd817e236679073372
Angelo404 <e.karountzos@hotmail.com>
2015-12-30 03:52:40 +0000

    adding threads

--- src/bluetooth/Client.py
+++ src/bluetooth/Client.py
@@ -7,9 +7,7 @@ class Client(Bluetooth):
 	def __init__(self):
 		super(self.__class__, self).__init__()
 
-		self.bd_addr = "01:23:45:67:89:ab"
-
-		self.port = 3
+		self.bd_addr = "54:35:30:D4:11:AE"
 
 		self.socket.connect((self.bd_addr, self.port))
 
--- src/bluetooth/Server.py
+++ src/bluetooth/Server.py
@@ -7,9 +7,6 @@ class Server(Bluetooth):
 	def __init__(self):
 		super(self.__class__, self).__init__()
 
-		print self.Scan()
-
-		self.port = 3
 		self.socket.bind(("01:23:45:67:89:ab",self.port))
 		self.socket.listen(1)
 		
