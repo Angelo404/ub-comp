083589c245ec480121d602c1d5a1abbedffa955f
mathieu <matsjoe1990@gmail.com>
2016-01-10 22:02:20 +0100

    start of making a context

---
+++ src/Context.py
@@ -0,0 +1,24 @@
+import threading
+
+
+class Context:
+
+    def __init__(self):
+        self.state = {}
+        self.stateLock = threading.Lock()
+        self.update("Sensors:Door:BedRoom", "open")
+        self.update("Sensors:Lights:Desk", "0")
+        self.update("Sensors:Door:BedRoom", "Closed")
+
+    def update(self, key, value):
+        with self.stateLock:
+            self.state[key] = value
+
+        self.onContextChanged(key, value)
+        self.printContext()
+
+    def onContextChanged(self, key, value):
+        pass
+
+    def printContext(self):
+        print "State: {}".format(self.state)
---
+++ src/Main.py
@@ -0,0 +1,9 @@
+from Context import Context
+
+
+def main():
+    context = Context()
+
+
+if __name__ == "__main__":
+    main()
