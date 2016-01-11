f5f80d77c1dbeb03ac322fd4e40357e6b6f2f0f8
mathieu <matsjoe1990@gmail.com>
2016-01-11 14:25:59 +0100

    log to file

--- src/Communication/Receiver.py
+++ src/Communication/Receiver.py
@@ -1,23 +1,26 @@
 from urllib2 import Request,urlopen
 from pi_switch import RCSwitchReceiver
+import logging
 
 def opened_door():
-    print "Door opened!"
+    logging.info("Door opened!")
     sendStateChange("Sensors:Door:Bedroom", "open")
 
 def sendStateChange(key, value):
     request = Request("http://localhost:5000/API/update/{}/{}".format(key, value))
     response = urlopen(request)
     responseText = response.read()
-    print responseText
+    logging.debug(responseText)
 
 
 receiver = RCSwitchReceiver()
 receiver.enableReceive(2)
+logging.basicConfig(filename="/home/pi/receiver_log.log", level=logging.DEBUG, format="%(asctime)s %(message)s")
 
 while True:
     if receiver.available():
         received_value = receiver.getReceivedValue()
+        logging.debug("Received value: {}".format(received_value))
         if received_value:
             if received_value == 2876233:
                 opened_door()
