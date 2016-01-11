9ab9d372f01630acf14551867fa9760231de01fe
mathieu <matsjoe1990@gmail.com>
2016-01-11 16:01:52 +0100

    enable light

--- src/Context.py
+++ src/Context.py
@@ -19,8 +19,13 @@ class Context:
     def play_sound(self):
         os.system("aplay /home/pi/police_s.wav")
 
+    def enable_light(self):
+        os.system("sudo /home/pi/bin/elro 2 B on")
+        self.update("Actuators:Lights:Desk", "on")
+
     def onContextChanged(self, key, value):
         if "Door" in key and value == "open":
+            self.enable_light()
             self.play_sound()
 
     def printContext(self):
