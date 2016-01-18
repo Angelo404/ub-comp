154e4969b528eef0ac5d246f563dd06780c15dc3
Guus Klinkenberg <g.klinkenberg@gmail.com>
2016-01-18 23:58:07 +0100

    Added base for API, needs to be split from alarms though

--- src/Alarm/Alarm.py
+++ src/Alarm/Alarm.py
@@ -1,73 +1,73 @@
 #!/usr/bin/python
 
-import threading
+from flask import Flask, render_template, send_from_directory
 import sqlite3
 import time
 from datetime import datetime
 from threading import Timer
 import spotify
-
 import SpotifyPlayer
+
+app = Flask(__name__)
+
+# Because calling this directly on the object does not work for some magic
+# reason
+def play_track(track_uri):
+    alarm.spotify.play_track(track_uri)
+
 
 class Alarm(object):
 
-	alarms = []
-	spotify = SpotifyPlayer.SpotifyPlayer()
-	db = None 
+    alarms = []
+    spotify = SpotifyPlayer.SpotifyPlayer()
+    db = None
 
-	def __init__(self):
-		self.connect_to_db('../sqlite.db') # Needs to be replaced with something from the config file
-		self.get_current_alarms_from_db()
-		# TODO needs something to restart the timers
+    def __init__(self):
+        # Needs to be replaced with something from the config file
+        self.connect_to_db('../sqlite.db')
+        self.get_current_alarms_from_db()
+        # TODO needs something to restart the timers
 
-	def connect_to_db(self, path):
-		self.db = sqlite3.connect(path)
-		self.db.row_factory = sqlite3.Row
+    def connect_to_db(self, path):
+        self.db = sqlite3.connect(path)
+        self.db.row_factory = sqlite3.Row
 
-	def get_current_alarms_from_db(self):
-		self.alarms = self.query_db("SELECT * FROM alarm WHERE repeat > 0 OR time > date('now')")
+    def start_stored_alarms(self):
+        for alarm in self.alarms:
+            Timer(1.0, play_track, [alarm['track_uri']]).start()
 
-	def query_db(self, query, args=(), one=False):
-	    cur = self.db.execute(query, args)
-	    rv = cur.fetchall()
-	    cur.close()
-	    return (rv[0] if rv else None) if one else rv
+    def get_current_alarms_from_db(self):
+        self.alarms = self.query_db(
+            "SELECT * FROM alarm WHERE repeat > 0 OR time > date('now')")
+        self.start_stored_alarms()
 
-	def playtrack(self):
-		play_track('spotify:track:0HJRAM7Gt9jXskuXjZeFX3')
+    def query_db(self, query, args=(), one=False):
+        cur = self.db.execute(query, args)
+        rv = cur.fetchall()
+        cur.close()
+        return (rv[0] if rv else None) if one else rv
 
-	def play_track(self, track_uri):
-		spotify.play_track(track_uri)
+    def playtrack(self):
+        play_track('spotify:track:0HJRAM7Gt9jXskuXjZeFX3')
 
-# Because calling this directly on the object does not work for some magic reason
-def play_track(track_uri):
-	alarm.spotify.play_track(track_uri)
+    def play_track(self, track_uri):
+        spotify.play_track(track_uri)
 
-track_uri = 'spotify:track:0HJRAM7Gt9jXskuXjZeFX3'
-alarm = Alarm()
+@app.route("/")
+def root():
+    return render_template("addalarm.html")
 
-# c = db.cursor()
-# result = c.execute('''SELECT * FROM alarm WHERE repeat > 0 OR time > date('now')''')
-# query = 'SELECT * FROM alarm'
-# print query
-# c.execute(query)
-# result = c.fetchall()
-# res = alarm.alarms
-# for row in res:
-	# print row['user']
-
+# track_uri = 'spotify:track:0rCuRc07y6l1kPYj0JSRg5'
+# alarm = Alarm()
 
 # Short test to see if Spotify is working in combination with Timer
-t = Timer(1.0, play_track, [track_uri])
+# t = Timer(1.0, play_track, [track_uri])
 # Alarms.append(t)
-t.start()
-time.sleep(10)
-# print spotify.isPlaying()
-# print Alarms
+# t.start()
 
-# Busy wait for spotify
-try:
-    while alarm.spotify.isPlaying():
-        pass
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
