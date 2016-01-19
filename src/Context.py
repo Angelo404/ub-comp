d1e79a053fac77c83679831382a21810a13cc1ac
mathieu <matsjoe1990@gmail.com>
2016-01-19 12:29:43 +0100

    check for the state of the sun (rise or set)

--- src/Context.py
+++ src/Context.py
@@ -1,7 +1,6 @@
 import os
 import threading
-import pygame
-
+from datetime import datetime
 
 class Context:
 
@@ -23,10 +22,26 @@ class Context:
         os.system("sudo /home/pi/bin/elro 2 B on")
         self.update("Actuators:Lights:Desk", "on")
 
+    def sunRised(self):
+        # with no information assume it is dark
+        if not self.state.has_key("Sensors:Sun:Rise"):
+            return False
+        if not self.state.has_key("Sensors:Sun:Set"):
+            return False
+        sunRise = datetime.strptime(self.state["Sensors:Sun:Rise"], "%Y-%m-%dT%H:%M:%S")
+        sunSet = datetime.strftime(self.state["Sensors:Sun:Set"], "%Y-%m-%dT%H:%M:%S")
+        now = datetime.now()
+        return sunRise < now < sunSet
+
+    def onDoorOpened(self):
+        lightOn = self.state["Actuators:Lights:Desk"] == "on"
+        sun_is_shining = self.sunRised()
+        if not lightOn and not sun_is_shining:
+            self.enable_light()
+
     def onContextChanged(self, key, value):
         if "Door" in key and value == "open":
-            self.enable_light()
-            self.play_sound()
+            self.onDoorOpened()
 
     def printContext(self):
         print "State: {}".format(self.state)
